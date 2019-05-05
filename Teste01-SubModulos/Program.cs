1dbf4190733ee0c34359643bc05187dd65c639bc
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2019-05-05 16:57:29 -0300

    Adicionar arquivos de projeto.

---
+++ Teste01-SubModulos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste01_SubModulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
