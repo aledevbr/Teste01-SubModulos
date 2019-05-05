e2361f73e8baf29079b38952fa16544c766c7627
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2019-05-05 17:11:10 -0300

    alteração no módulo e submódulo

--- Teste01-SubModulos/Program.cs
+++ Teste01-SubModulos/Program.cs
@@ -7,6 +7,7 @@ namespace Teste01_SubModulos
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Passo 2");
         }
     }
 }
